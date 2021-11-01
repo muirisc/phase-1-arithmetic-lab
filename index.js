0098f342f4a7efb46a778a1e2e7db9ab30585872
muirisc <91299876+muirisc@users.noreply.github.com>
2021-11-01 16:24:57 -0400

    commit tree 9f34f07742283194b9886a1d49168d6e67c3dc17

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+// multiply code
+let num1 = 62
+let num2 = 1
+let multiply = num1 * num2
+
+//random
+let random = Math.ceil(Math.random());
+
+// Mod
+
+let num3 = 32
+let num4 = 8
+let mod = num3 / num4 
+
+// max
+
+let max = Math.max(1, 3, 5, 10, 20)
